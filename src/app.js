4bab01400ba94fa4df313f1a1cc953a376e21929
rkiang <ryanlkiang@gmail.com>
2018-05-14 10:49:18 -0500

    created app.js and called in the new dependencies

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = 3000;
