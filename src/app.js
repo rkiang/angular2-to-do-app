9ef13a060b9cf92c39ef9d56273aeebfb17deac4
rkiang <ryanlkiang@gmail.com>
2018-05-15 10:43:27 -0500

    setup backend server

--- src/app.js
+++ src/app.js
@@ -7,3 +7,18 @@ const mongoose = require('mongoose');
 const app = express();
 
 const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send("Invalid Page");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);    
+});
