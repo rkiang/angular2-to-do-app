9251bab6c5aa7d74edb6970c9ed657a10b6df5fa
rkiang <ryanlkiang@gmail.com>
2018-05-22 11:21:57 -0500

    imported todoDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodoDataService } from './todo-data.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TodoDataService
   ],
   providers: [],
   bootstrap: [AppComponent]
